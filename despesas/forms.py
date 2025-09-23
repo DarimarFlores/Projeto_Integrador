7d776a8842bc5e23b738cab25c1272897fc6675e
DarimarFlores <darimar.flores1984@gmail.com>
2025-09-22 21:17:07 -0300

    edicao do app despesas, adicione frenquencia e tipo

--- despesas/forms.py
+++ despesas/forms.py
@@ -4,7 +4,7 @@ from .models import Despesa
 class DespesaForm(forms.ModelForm):
     class Meta:
         model = Despesa
-        fields =['nome','valor','categoria','descricao','data_pagamento','pago']
+        fields =['nome','valor','frequencia','tipo','data_pagamento','pago']
         widgets = {
             'data_pagamento': forms.DateInput(attrs={'type':'date'}),
         }
--- despesas/models.py
+++ despesas/models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Despesa(models.Model):
+    FREQUENCIA_CHOICES = [
+        ('D', 'Diária'), 
+        ('S', 'Semanal'),
+        ('M', 'Mensal'),
+        ('SM', 'Semestral'),
+        ('A', 'Anual'),         
+    ]
+
+    TIPO_CHOICES = [
+        ('GF', 'Gasto Formiga'),
+        ('FX', 'Fixa'),
+        ('V', 'Variável'),
+        ('I', 'Investimento'),
+    ]
+
     nome = models.CharField(max_length=100, null=True, blank=True)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-    categoria = models.CharField(max_length=60)
-    descricao = models.TextField(null=True, blank=True)
+    frequencia = models.CharField(max_length=2, choices=FREQUENCIA_CHOICES, default='M')
+    tipo = models.CharField(max_length=2, choices=TIPO_CHOICES, default='V')
     data_pagamento = models.DateField(null=True, blank=True)
     pago = models.BooleanField(default=False)
+    
 
     def __str__(self):
-        return f"{self.nome} - R$ {self.valor}"
+        return self.nome
