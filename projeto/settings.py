5e7390878ae889f33839b064d88dac15fb609288
DarimarFlores <darimar.flores1984@gmail.com>
2025-11-27 20:36:33 -0300

    conexao com o banco de dados- docker

--- projeto/settings.py
+++ projeto/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projetointegrador',  # nome do banco
+        'USER': 'postgres',            # seu usuário
+        'PASSWORD': 'e3d4r35tsq354d2E',       # coloque sua senha real
+        'HOST': 'localhost',           # ou IP do servidor
+        'PORT': '5432',                # porta padrão do Postgres
     }
 }
 
